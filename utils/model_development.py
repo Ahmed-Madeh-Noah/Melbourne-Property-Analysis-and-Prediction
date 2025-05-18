513c66eb0294cfeebf1398c96d8830044436b657
Ahmed Madeh Noah <ahmed.madeh.noah@gmail.com>
2025-05-19 00:56:26 +0300

    Add `model_development.py` functionality

--- utils/model_development.py
+++ utils/model_development.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import numpy as np
 from sklearn.base import BaseEstimator
-from sklearn.metrics import mean_absolute_error, mean_squared_error, root_mean_squared_error, r2_score, \
-    mean_absolute_percentage_error
+from sklearn.metrics import mean_absolute_error, mean_squared_error, root_mean_squared_error
+from sklearn.metrics import r2_score, mean_absolute_percentage_error
 import joblib
+from sklearn.linear_model import LinearRegression, Ridge, ElasticNet, BayesianRidge, ARDRegression
+from sklearn.svm import SVR
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.neighbors import KNeighborsRegressor
 
 
 def load_split_datasets() -> tuple:
@@ -31,4 +36,20 @@ def train_model(X_train: pd.DataFrame, X_test: pd.DataFrame, y_train: np.array, 
 
 
 if __name__ == "__main__":
-    pass
+    X_train, X_test, y_train, y_test = load_split_datasets()
+    evaluation = pd.DataFrame(columns=["mae", "mse", "rmse", "r2", "mape"])
+    evaluation.index.name = "Model"
+    train_model(X_train, X_test, y_train, y_test, LinearRegression(n_jobs=-1), evaluation=evaluation, save=True)
+    train_model(X_train, X_test, y_train, y_test, Ridge(random_state=42), evaluation=evaluation, save=True)
+    train_model(X_train, X_test, y_train, y_test, ElasticNet(alpha=0.1), evaluation=evaluation, save=True)
+    train_model(X_train, X_test, y_train, y_test, SVR(kernel="linear"), evaluation=evaluation, save=True)
+    train_model(X_train, X_test, y_train, y_test, DecisionTreeRegressor(), evaluation=evaluation, save=True)
+    train_model(X_train, X_test, y_train, y_test, RandomForestRegressor(random_state=42, n_jobs=-1),
+                evaluation=evaluation, save=True)
+    train_model(X_train, X_test, y_train, y_test, GradientBoostingRegressor(learning_rate=0.2, random_state=42),
+                evaluation=evaluation, save=True)
+    train_model(X_train, X_test, y_train, y_test,
+                KNeighborsRegressor(n_neighbors=9, weights="distance", metric="manhattan", n_jobs=-1),
+                evaluation=evaluation, save=True)
+    train_model(X_train, X_test, y_train, y_test, BayesianRidge(), evaluation=evaluation, save=True)
+    train_model(X_train, X_test, y_train, y_test, ARDRegression(), evaluation=evaluation, save=True)
